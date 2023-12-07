7572a358f7a8b05b8ba9a2b0c28addeb023cc3cf
jan Jak pijak <kowalalex204@gmail.com>
2023-12-08 02:19:02 +0800

    project creeated

--- BranchingAlgorithms/Program.cs
+++ BranchingAlgorithms/Program.cs
@@ -10,6 +10,37 @@ namespace FirstSemester
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1.Написать программу для определения подходящего возраста для вступления в брак, используя следующее соображение: возраст девушки равен половине возраста мужчины плюс 7, возраст мужчины определяется соответственно как удвоенный возраст девушки минус 14. Данные для проверки работы программы задать самостоятельно.");
+
+            Console.WriteLine();
+
+
+            int maleAge, femaleAge;
+            double correctMaleAge, correctFemaleAge;
+
+            Console.WriteLine("Введите возраст мужчины");
+            maleAge = int.Parse(Console.ReadLine());
+            correctFemaleAge = maleAge / 2 + 7;
+            if (correctFemaleAge < 18)
+            {
+                Console.WriteLine("невозможно вычислить корректный возраст");
+            }
+            else 
+            {
+                Console.WriteLine($"корректный возраст женщины для вступления в брак с данным мужчинной ={correctFemaleAge}");
+            }
+            Console.WriteLine("Введите возраст женщины");
+            femaleAge = int.Parse(Console.ReadLine());
+            correctMaleAge = femaleAge * 2 - 14;
+            if (correctFemaleAge < 18)
+            {
+                Console.WriteLine("невозможно вычислить корректный возраст");
+            }
+            else
+            {
+                Console.WriteLine($"корректный возраст мужчины для вступления в брак с данной женщиной= {correctMaleAge}");
+            }
+            Console.ReadLine();
         }
     }
 }
