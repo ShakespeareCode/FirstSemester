ad197c0b9c71de298ae73cc5a87cfef20579d2b0
jan Jak pijak <kowalalex204@gmail.com>
2023-12-08 22:38:47 +0800

    Project added.

---
+++ Индивидуальная задача 1 РАЗДЕЛ 2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace Индивидуальная_задача_1_РАЗДЕЛ_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Написать программу, которая при вводе числа в диапазоне от 1 до 20 nдобавляет к нему слово гривна в правильной форме. Например, 1гривна,2 гривны,5 гривен.");
+            WriteLine("");
+            WriteLine("введите число от 1 до 20");
+            uint inputValue = uint.Parse(ReadLine());
+            switch (inputValue)
+            {
+                case 1:
+                    WriteLine($"{inputValue} гривна");
+                    break;
+                case uint temp when temp < 5 && temp > 1:
+                    WriteLine($"{inputValue} гривны");
+                    break;
+                case uint temp when temp >= 5 && temp <= 20:
+                    WriteLine($"{inputValue} гривен");
+                    break;
+                default:
+                    WriteLine("введенны некорректные данные");
+                    break;
+            }
+            ReadKey();
+        }
+    }
+}
