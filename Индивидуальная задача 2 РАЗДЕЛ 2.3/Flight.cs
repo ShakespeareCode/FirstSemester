0a21407a62783d2556e3a28d90930cbf44d4f357
jan Jak pijak <kowalalex204@gmail.com>
2023-12-09 01:45:59 +0800

    Flight sructures added.

---
+++ Индивидуальная задача 2 РАЗДЕЛ 2.3/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Индивидуальная_задача_2_РАЗДЕЛ_2._3
+{
+    internal class Flight
+    {
+        public string City { get; }
+        public uint CostOfTwoDirection { get; }
+        public uint CostOfOneDirection { get; }
+        public Flight(string city, uint costOfOneDirection, uint costOfTwoDirection)
+        {
+            City = city;
+            CostOfOneDirection = costOfOneDirection;
+            CostOfTwoDirection = costOfTwoDirection;
+        }
+
+    }
+}
